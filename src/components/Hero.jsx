68829d7a1e71198b427835cf799e5427bde5e749
Norman Cade <norman_cade32@yahoo.com>
2023-05-31 16:53:40 -0400

    init files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <img src={logo} alt="logo" className="w-28 object-contain" />
         <button
           type="button"
-          onClick={() => window.open("https://google.com")}
+          onClick={() => window.open("https://github.com/djcade32/ai-summarizer-website")}
           className="black_btn"
         >
           GitHub
